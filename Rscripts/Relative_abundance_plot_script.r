732168f285fb618d91e4552a7b78cd581e3e80c4
Luthando Madonsela <91069314+LuthandoMadonsela@users.noreply.github.com>
2025-09-01 23:29:13 +0200

    Update Relative_abundance_plot_script.r

--- Rscripts/Relative_abundance_plot_script.r
+++ Rscripts/Relative_abundance_plot_script.r
@@ -18,7 +18,6 @@ melted_data$Sample <- factor(melted_data$Sample,levels=unique(melted_data$Sample
 
 #Generate a palette with distinctive colours
 library(randomcoloR)
-library(viridis)
 n <- 800
 palette <- distinctColorPalette(n)
 
@@ -35,4 +34,5 @@ barplot = ggplot(melted_data, aes(x = Sample, fill = forcats::fct_rev(variable),
   labs(x = "", y = "Relative Abundance (%)", fill = "OTU")
 
 #Show plot
+
 barplot
